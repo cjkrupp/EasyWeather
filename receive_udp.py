302807255c2d7bc73503b1571677f66dc0056ecc
cjkrupp <cjkrupp@gmail.com>
2021-03-22 15:03:13 -0400

    update

--- receive_udp.py
+++ receive_udp.py
@@ -23,95 +23,114 @@ import array
     NOAA 19 - 137.1000 MHz
 
 '''
+def receive_data_UDP():
+    # localhost
+    UDP_IP = "127.0.0.1"
 
-# localhost
-UDP_IP = "127.0.0.1"
+    # GQRX default port
+    UDP_PORT = 7355
 
-# GQRX default port
-UDP_PORT = 7355
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((UDP_IP, UDP_PORT))
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+    # initialize as a byte
+    data = b'0'
 
-# initialize as a byte
-data = b'0'
+    data_times = 1024
 
-data_times = 1024
+    f = open("radio-capture.dat", "ab")
+    time_end = time.time() + 60*1
+    while time.time() < time_end:
+    #while data_times >= 0:
+        data_temp, addr = sock.recvfrom(1024) # buffer size of 1024 bytes
+        data = data + data_temp
+        data_times -= 1
+        #print("Received data: %s" % data)
 
-f = open("radio-capture.dat", "ab")
-time_end = time.time() + 60 #60*15
-while time.time() < time_end:
-#while data_times >= 0:
-    data_temp, addr = sock.recvfrom(1024) # buffer size of 1024 bytes
-    data = data + data_temp
-    data_times -= 1
-    #print("Received data: %s" % data)
+    # generate t from 1 to len(data)
+    t = list(range(len(data)))
 
-# generate t from 1 to len(data)
-t = list(range(len(data)))
+    # Convert continuous stream of data into array of bytes
+    data = bytearray(data)
 
-# Convert continuous stream of data into array of bytes
-data = bytearray(data)
-
-# Delete the last element to make data array have an even number of elements
-del data[len(data)-1:]
-# Do the same as above for t
-del t[len(t)-1:]
-
-
-print(len(data))
-print('='+str(len(data))+'h')
-
-# Turn array of bytes into an array of signed words
-data_16bit = array.array('h', data)
-# Because data comes in in reverse order, swap the bytes two by two for each word
-data_16bit.byteswap()
-
-# Create a new t to match our array of words (shortened to half the size in bytes)
-t = list(range(len(data_16bit)))
-
-f.write(data_16bit)
-f.close()
-
-print(list(data_16bit))
-# plotting the data
-plt.plot(t,data_16bit) 
-plt.show()
+    # Delete the last element to make data array have an even number of elements
+    del data[len(data)-1:]
+    # Do the same as above for t
+    del t[len(t)-1:]
 
 
-# ###########################################
-# Below is code for testing only \/ \/
-# ###########################################
+    #print(len(data))
+    #print('='+str(len(data))+'h')
 
-def hilbert(data):
-    analytic_signal = scipy.signal.hilbert(data)
-    amplitude_envelope = np.abs(analytic_signal)
-    return amplitude_envelope
+    # Turn array of bytes into an array of signed words
+    data_16bit = array.array('h', data)
+    # Because data comes in in reverse order, swap the bytes two by two for each word
+    data_16bit.byteswap()
 
-data_hilbert = hilbert(data_16bit)
+    # Create a new t to match our array of words (shortened to half the size in bytes)
+    t = list(range(len(data_16bit)))
 
-plt.plot(t,data_16bit,t,data_hilbert) 
-plt.show()
+    #f.write(data_16bit)
+    f.close()
+
+    #print(list(data_16bit))
+    # plotting the data
+    plt.figure(1)
+    plt.plot(t,data_16bit) 
+    
+    #plt.show()
+    
+    #fig = plt.gcf()
+    #fig.set_size_inches(15,10)
+    #plt.savefig('input.png')
+
+    # ###########################################
+    # Below is code for testing only \/ \/
+    # ###########################################
+
+    def hilbert(data):
+        analytic_signal = scipy.signal.hilbert(data)
+        amplitude_envelope = np.abs(analytic_signal)
+        return amplitude_envelope
+
+    data_hilbert = hilbert(data_16bit)
+
+    plt.figure(2)
+    plt.plot(t,data_16bit,t,data_hilbert) 
+    
+    #plt.show()
+    #fig = plt.gcf()
+    #fig.set_size_inches(15,10)
+    #plt.savefig('demod.png')
 
 
+    time_step = 1/48000
 
-time_step = 1/48000
+    #total_data = (np.sin(2 * np.pi*1600*time_vec))
+    # The FFT of the signal
+    sig_fft = fftpack.fft(data_16bit)
 
-#total_data = (np.sin(2 * np.pi*1600*time_vec))
-# The FFT of the signal
-sig_fft = fftpack.fft(data_16bit)
+    # And the power (sig_fft is of complex dtype)
+    power = np.abs(sig_fft)**2
+    # The corresponding frequencies
+    sample_freq = fftpack.fftfreq(len(data_16bit), d=time_step)
 
-# And the power (sig_fft is of complex dtype)
-power = np.abs(sig_fft)**2
-# The corresponding frequencies
-sample_freq = fftpack.fftfreq(len(data_16bit), d=time_step)
+    # Plot the FFT power
+    #plt.figure(figsize=(6, 5))
+    plt.figure(3)
+    plt.plot(sample_freq, power)
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('plower')
+    
+    plt.show()
+    #fig = plt.gcf()
+    #fig.set_size_inches(15,10)
+    #plt.savefig('FFT.png')
+    
+    print("[receive_udp] Data saved to radio-capture.dat, input.png, demod.png, FFT.png")
+    
+    return data_16bit
 
-# Plot the FFT power
-#plt.figure(figsize=(6, 5))
-plt.plot(sample_freq, power)
-plt.xlabel('Frequency [Hz]')
-plt.ylabel('plower')
-plt.show()
 
 
 
